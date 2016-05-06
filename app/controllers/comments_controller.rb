8527e50b9b0810b2ab80c5598d28895448fcaa69
COLIN <reckocp@gmail.com>
2016-05-06 15:13:17 -0500

    Comment form working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to :back, notice: 'Comment was successfully created.' }
         format.js   { }
         format.json { render :show, status: :created, location: @comment }
       else
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160506185135) do
+ActiveRecord::Schema.define(version: 20160506191943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string   "title"
     t.text     "body"
     t.integer  "user_id"
     t.integer  "post_id"
